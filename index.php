34364f1530eff298e0119d3dde7633ccf1e687d4
root <root@localhost.localdomain>
2016-04-03 00:15:49 +0000

    Modificacion al Index y creacion de nuevos archivos

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 echo"Nuevo Archivo"; 
  ?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>Document</title>
+ </head>
+ <body>
+ 	<h2>Nueva Modificacion</h2>
+ </body>
+ </html>
